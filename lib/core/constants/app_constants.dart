e4f2c0dd2ef764c8cdc2f178a6c381a294d394e5
will codyy <wilberthleal001@ggmail.com>
2026-07-02 11:43:00 -0300

    Polimento visual global do sistema

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,5 +2,5 @@ class AppConstants {
   AppConstants._();
 
   static const String appName = 'Açougue do Leleco';
-  static const String appVersion = '3.6.4';
+  static const String appVersion = '3.6.5';
 }
